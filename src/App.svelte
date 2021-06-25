1656b5ca72ab62df3bc33e8b03d39fb677c73adb
Colin Bate <colin.bate@orbis.com>
2021-06-25 14:17:17 -0300

    Initial version

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
 	import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
-	export let name: string;
+	import Timer from './Timer.svelte';
+	const params = new URL(window.location.toString()).searchParams;
+	let title = params.get('title') ?? 'Game Time';
+	let target = params.get('time') ?? '0';
+	const ts = parseInt(target, 10) * 1000;
 </script>
-<style>
-	.custom-style {
-		@apply italic;
-	}
-</style>
 <Tailwindcss />
 <ModeSwitcher />
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
-	<p class="custom-style mt-[3rem]">
-		Visit the
-		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
+	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">{title}</h1>
+	<div class="text-2xl mt-12 font-bold">
+		<Timer endDate={ts}/>
+	</div>
 </main>
---
+++ src/Timer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { onDestroy } from 'svelte';
+  export let endDate: number = 0;
+  let current = Date.now();
+  function formatDuration(ms: number) {
+    if (ms <= 0) {
+      return '0d 00:00:00';
+    }
+    const secs = Math.floor(ms / 1000);
+    const mins = Math.floor(secs / 60);
+    const srem = Math.floor(secs % 60);
+    const hour = Math.floor(mins / 60);
+    const mrem = Math.floor(mins % 60);
+    const days = Math.floor(hour / 24);
+    const hrem = Math.floor(hour % 24);
+    return `${days}d ${hrem < 10 ? '0' : ''}${hrem}:${mrem < 10 ? '0' : ''}${mrem}:${srem < 10 ? '0' : ''}${srem}`;
+  }
+  $: timeStr = formatDuration(endDate - current);
+  let frame;
+  function countdown() {
+    frame = requestAnimationFrame(countdown);
+    current = Date.now();
+  }
+  onDestroy(() => cancelAnimationFrame(frame));
+  countdown();
+</script>
+<div>
+{timeStr}
+</div>
